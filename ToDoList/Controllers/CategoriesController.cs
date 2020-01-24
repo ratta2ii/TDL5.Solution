f2758fbdd9134964032aa9f812e847f4f33200c9
Trevor Mackin and Wei Dai <wei@>
2020-01-23 16:18:06 -0800

    Add authentication for Categories

--- ToDoList/Controllers/CategoriesController.cs
+++ ToDoList/Controllers/CategoriesController.cs
@@ -3,23 +3,38 @@ using ToDoList.Models;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization; //  will allow us to actually authorize users.
+using Microsoft.AspNetCore.Identity; //  enables controller to interact with users from the DB
+using System.Threading.Tasks; //  so we can call async methods
+using System.Security.Claims; // important for using what is called Claim Based Authorization
 
 namespace ToDoList.Controllers
 {
+    [Authorize]
     public class CategoriesController : Controller
     {
         private readonly ToDoListContext _db;
+        private readonly UserManager<ApplicationUser> _userManager; // authentication
 
-        public CategoriesController(ToDoListContext db)
+        public CategoriesController(UserManager<ApplicationUser> userManager, ToDoListContext db)
         {
+            _userManager = userManager;
             _db = db;
         }
 
-        public ActionResult Index()
+        public async Task<ActionResult> Index()
         {
-            List<Category> model = _db.Categories.ToList();
-            return View(model);
+            var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var currentUser = await _userManager.FindByIdAsync(userId);
+            var userCategories = _db.Categories.Where(entry => entry.User.Id == currentUser.Id);
+            return View(userCategories);
         }
+
+        // public ActionResult Index()
+        // {
+        //     List<Category> model = _db.Categories.ToList();
+        //     return View(model);
+        // }
 
         public ActionResult Create()
         {
@@ -27,12 +42,27 @@ namespace ToDoList.Controllers
         }
 
         [HttpPost]
-        public ActionResult Create(Category category)
+        public async Task<ActionResult> Create(Category category, int ItemId)
         {
+            var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var currentUser = await _userManager.FindByIdAsync(userId);
+            category.User = currentUser;
             _db.Categories.Add(category);
+            if (ItemId != 0)
+            {
+                _db.CategoryItem.Add(new CategoryItem() { ItemId = ItemId, CategoryId = category.CategoryId });
+            }
             _db.SaveChanges();
             return RedirectToAction("Index");
         }
+
+        // [HttpPost]
+        // public ActionResult Create(Category category)
+        // {
+        //     _db.Categories.Add(category);
+        //     _db.SaveChanges();
+        //     return RedirectToAction("Index");
+        // }
 
 
         public ActionResult Details(int id)
@@ -43,7 +73,7 @@ namespace ToDoList.Controllers
                 .FirstOrDefault(category => category.CategoryId == id);
             return View(thisCategory);
         }
-     
+
 
         public ActionResult Edit(int id)
         {
--- ToDoList/Models/Category.cs
+++ ToDoList/Models/Category.cs
@@ -6,8 +6,8 @@ namespace ToDoList.Models
     {
         public int CategoryId { get; set; }
         public string Name { get; set; }
+        public virtual ApplicationUser User { get; set; } //login
         public virtual ICollection<CategoryItem> Items { get; set; }
-
         public Category()
         {
             this.Items = new HashSet<CategoryItem>();
